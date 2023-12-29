a4037358998a463d3b5f2980b450d1e6545eb351
Augustin Boissier <augustinboissier@Augustins-MacBook-Air.local>
2023-12-29 23:59:56 +0100

    added TD4

---
+++ TD4/src/Cell.java
@@ -0,0 +1,128 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+/**
+ * Cette classe modélise une case du labyrinthe
+ */
+public abstract class Cell {
+
+	private boolean isMarked; // si la case est marquée
+	private boolean isExit; // si la case est la sortie du labyrinthe
+	/** liste des cases voisines de la cellule */
+	private ArrayList<Cell> neighbors; // 
+	private ArrayList<Boolean> walls; // pour chacune des cases voisines, s'il y a un mur dans sa direction
+	protected Maze maze; // référence au labyrinthe (pour la visualisation)
+
+
+	Cell() {}
+	
+	/**
+	 * Initialise une case du labyrinthe
+	 */
+	Cell(Maze maze) { 
+		isMarked = false;
+		isExit = false;
+		neighbors = new ArrayList<>();
+		walls = new ArrayList<>();
+		this.maze = maze;
+	}
+
+	/**
+	 * Renvoie la liste des cases voisines (accessibles ou pas, selon la valeur de l'argument)
+	 * 
+	 * @return renvoie uniquement la liste des cases vers lesquelles un passage existe, si ignoreWalls==false. <br>
+	 *         Si ignoreWalls==true, alors toutes les cases voisines sont renvoyées.
+	 */
+	List<Cell> getNeighbors(boolean ignoreWalls) {
+		if(ignoreWalls)
+			return new ArrayList<>(neighbors);
+		else
+			return neighbors.stream().filter(cell -> this.hasPassageTo(cell)).collect(Collectors.toList());
+	}
+
+	/**
+	 * Teste si la case 'c' est une voisine et il n'y a pas de mur dans sa direction
+	 * 
+	 * @return  true ssi la case 'c' est une voisine et il n'y a pas de mur dans sa direction
+	 */
+	boolean hasPassageTo(Cell c) {
+		if(c == null || this.neighbors.indexOf(c) == -1 || c.neighbors.indexOf(this) == -1)
+			return false;
+
+		return !this.walls.get(this.neighbors.indexOf(c));
+	}
+
+	/**
+	 * Supprime le mur dans la direction de la case 'c' (si la case courante et la case 'c' sont adjacentes)
+	 */
+	void breakWall(Cell c) {
+		if(c == null || this.neighbors.indexOf(c) == -1 || c.neighbors.indexOf(this) == -1)
+			throw new IllegalArgumentException("cells are not neighbors");
+
+		this.walls.set(this.neighbors.indexOf(c), false);
+		c.walls.set(c.neighbors.indexOf(this), false);
+	}
+
+	/**
+	 * Teste si la case est marquée
+	 * 
+	 * @return  true ssi ssi la case est marquée
+	 */
+	boolean isMarked() {
+		return isMarked;
+	}
+
+	/**
+	 * Ajoute une marque si b==true, enlève la marque si b==false
+	 */
+	void setMarked(boolean b) {
+		isMarked = b;
+	}
+
+	/**
+	 * Teste si la case est une sortie
+	 * @return  true ssi la case est une sortie
+	 */
+	boolean isExit() {
+		return isExit;
+	}
+
+	/**
+	 * Déclare la case comme sortie si b==true, <br> déclare la case comme non-sortie si b==false
+	 */
+	void setExit(boolean b) {
+		isExit = b;
+	}
+
+	/**
+	 * Teste la case est isolée (i.e. a des murs dans toutes les directions)
+	 * 
+	 * @return  true ssi la case est isolée (i.e. a des murs dans toutes les directions)
+	 */
+	boolean isIsolated() {
+		return getNeighbors(false).isEmpty();
+	}
+
+	/**
+	 * Rajoute une case dans la liste des cases voisines
+	 */
+	void addNeighbor(Cell n) {
+		neighbors.add(n);
+		walls.add(true);
+	}
+
+	/**
+	 * Teste s'il existe un chemin de la case actuelle vers une sortie
+	 * 
+	 * @return  true ssi il existe un chemin de la case actuelle vers une sortie
+	 */
+	abstract boolean searchPath();
+
+	/**
+	 * Génère un labyrinthe parfait par rebroussement récursif
+	 */
+	abstract void generateRec();
+	
+}
---
+++ TD4/src/TD4.java
@@ -0,0 +1,351 @@
+/**
+ *  TD4. Rebroussement, solution et génération de labyrinthes <br>
+ * Ce fichier contient 2 classes: <br>
+ * 	- ExtendCell fournit à une case du labyrinthe des opérations pour calcuelr un chemin vers la sortier et générer un labyrinthe de manière récursive <br>
+ * 	- Maze modélise un labyrinthe.
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.io.IOException;
+
+import java.nio.charset.StandardCharsets;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * Cette classe permet d'étendre et enrichir la représentation d'une case du labyrinthe. <br>
+ * Elle fournit à une case les opérations permettant de: <br>
+ * -) trouver un chemin vers la sortie <br>
+ * -) Génerer un labyrinthe de manière récursive
+ * 
+ */
+class ExtendedCell extends Cell {
+	
+	public ExtendedCell(Maze maze) {
+		super(maze);
+	}
+	
+	// Question 1
+
+	/**
+	 * Teste s'il existe un chemin de la case actuelle vers une sortie
+	 * 
+	 * @return  true ssi il existe un chemin de la case actuelle vers une sortie
+	 */
+	boolean searchPath() {
+		maze.slow(); // ralentit l'animation de la recherche (pour aider à debugger)
+
+		//throw new Error("Methode searchPath() a completer (Question 1)");
+		
+		setMarked(true);
+		if(isExit()) return true;
+
+		for(Cell c : getNeighbors(false)) {
+			if(!c.isMarked() && c.searchPath()) return true;
+		}
+
+		setMarked(false);
+		return false;
+	}
+
+	// Question 2
+
+	/**
+	 * Génère un labyrinthe parfait par rebroussement récursif
+	 */
+	void generateRec() {
+		maze.slow();
+
+		throw new Error("Methode generateRec() a completer (Question 2)");
+	}
+
+}
+
+/**
+ * Cette classe modélise un labyrinthe
+ */
+class Maze {
+
+	private int height, width;
+	/** La grille (tableau de cellules) représentant le labyrinthe */
+	private Cell[][] grid;
+
+
+	// Question 3
+
+	/**
+	 * Génère un labyrinthe parfait par rebroussement itératif
+	 */
+	void generateIter(int selectionMethod) {
+		Bag cells = new Bag(selectionMethod);
+		cells.add(getFirstCell());
+
+		while(!cells.isEmpty()) {
+			slow();
+
+			 throw new Error("Methode generateIter() a completer (Question 3)");
+
+		}
+
+	}
+
+
+	// Question 4
+	
+	/**
+	 * Génère un labyrinthe avec l'algorithme de Wilson
+	 */
+	void generateWilson() {
+		throw new Error("Methode generateWilson() a completer (Question 4)");
+	}
+
+	/**
+	 * Renvoie la cellule avec les coordonnées (i, j)
+	 * 
+	 * @return  la cellule avec les coordonnées (i, j)
+	 */
+	Cell getCell(int i, int j) {
+		if(i < 0 || i >= height || j < 0 || j >= width)
+			throw new IllegalArgumentException("invalid indices");
+
+		return grid[i][j];
+	}
+
+	/**
+	 * Renvoie la cellule avec les coordonnées (0, 0)
+	 * 
+	 * @return  la cellule avec les coordonnées (0, 0)
+	 */
+	Cell getFirstCell() {
+		return getCell(0, 0);
+	}
+
+	// traduit coordonnées en numéro de cellule
+	int coordToInt(int i, int j) {
+		if(i < 0 || i >= height || j < 0 || j >= width)
+			throw new IndexOutOfBoundsException();
+
+		return i*width + j;
+	}
+
+	// traduit un numéro de cellule en Coordinate
+	Coordinate intToCoord(int x) {
+		if(x < 0 || x >= height*width)
+			throw new IndexOutOfBoundsException();
+
+		return new Coordinate(x/width, x%width);
+	}
+
+
+	// ralentit l'affichage du labyrinthe si une fenêtre graphique est ouverte
+	void slow(){
+		if (frame == null) return;
+
+		try {
+			Thread.sleep(10);
+			frame.repaint();
+		} catch (InterruptedException e) {}
+	}
+
+	private MazeFrame frame;
+	private static final int step = 20;
+
+	Maze(int height, int width) {
+		this(height, width, true);
+	}
+
+	Maze(int height, int width, boolean window) {
+		if((height <= 0) || (width <= 0))
+			throw new IllegalArgumentException("height and width of a Maze must be positive");
+
+		this.height = height;
+		this.width = width;
+
+		grid = new Cell[height][width];
+
+		for(int i = 0; i < height; ++i)
+			for(int j = 0; j < width; ++j)
+				grid[i][j] = new ExtendedCell(this);
+
+		for(int i = 0; i < height; ++i) {
+			for(int j = 0; j < width; ++j) {
+				if(i < height - 1) {
+					grid[i][j].addNeighbor(grid[i+1][j]);
+					grid[i+1][j].addNeighbor(grid[i][j]);
+				}
+
+				if(j < width - 1) {
+					grid[i][j].addNeighbor(grid[i][j+1]);
+					grid[i][j+1].addNeighbor(grid[i][j]);
+				}
+			}
+		}
+
+		grid[height-1][width-1].setExit(true);
+
+		if(window)
+			frame = new MazeFrame(grid, height, width, step);
+	}
+
+	Maze(String path) throws IOException {
+		this(Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8));
+	}
+
+	Maze(String path, boolean window) throws IOException {
+		this(Files.readAllLines(Paths.get(path), StandardCharsets.UTF_8), window);
+	}
+
+	Maze(List<String> lines) {
+		this(lines, true);
+	}
+
+	Maze(List<String> lines, boolean window) {
+		if(lines.size() < 2)
+			throw new IllegalArgumentException("too few lines");
+
+		this.height = Integer.parseInt(lines.get(0));
+		this.width = Integer.parseInt(lines.get(1));
+
+		this.grid = new Cell[height][width];
+		for(int i = 0; i < height; ++i)
+			for(int j = 0; j < width; ++j)
+				grid[i][j] = new ExtendedCell(this);
+
+		for(int i = 0; i < height; ++i) {
+			for(int j = 0; j < width; ++j) {
+				if(i < height - 1) {
+					grid[i][j].addNeighbor(grid[i+1][j]);
+					grid[i+1][j].addNeighbor(grid[i][j]);
+				}
+
+				if(j < width - 1) {
+					grid[i][j].addNeighbor(grid[i][j+1]);
+					grid[i][j+1].addNeighbor(grid[i][j]);
+				}
+			}
+		}
+
+		grid[height-1][width-1].setExit(true);
+
+		int i = 0;
+		int j = 0;
+
+		for(String line : lines.subList(2, lines.size())) {
+
+			for(int k = 0; k < line.length(); ++k) {
+				switch(line.charAt(k)) {
+					case 'N':
+						grid[i][j].breakWall(grid[i-1][j]);
+						break;
+					case 'E':
+						grid[i][j].breakWall(grid[i][j+1]);
+						break;
+					case 'S':
+						grid[i][j].breakWall(grid[i+1][j]);
+						break;
+					case 'W':
+						grid[i][j].breakWall(grid[i][j-1]);
+						break;
+					case '*':
+						grid[i][j].setMarked(true);
+						break;
+					default:
+						throw new IllegalArgumentException("illegal character");
+				}
+			}
+			++j;
+			if(j >= width) {
+				j = 0;
+				++i;
+			}
+		}
+
+		if(window)
+			frame = new MazeFrame(grid, height, width, step);
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+
+		sb.append(height);
+		sb.append('\n');
+		sb.append(width);
+		sb.append('\n');
+
+		for(int i = 0; i < height; ++i) {
+			for(int j = 0; j < width; ++j) {
+				if(i > 0 && grid[i][j].hasPassageTo(grid[i-1][j]))
+					sb.append('N');
+				if(j < width-1 && grid[i][j].hasPassageTo(grid[i][j+1]))
+					sb.append('E');
+				if(i < height-1 && grid[i][j].hasPassageTo(grid[i+1][j]))
+					sb.append('S');
+				if(j > 0 && grid[i][j].hasPassageTo(grid[i][j-1]))
+					sb.append('W');
+				if(grid[i][j].isMarked())
+					sb.append('*');
+				sb.append('\n');
+			}
+		}
+
+		return sb.toString();
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if(!(o instanceof Maze))
+			return false;
+		Maze that = (Maze)o;
+
+		return this.toString().equals(that.toString());
+	}
+
+	@Override
+	public int hashCode() {
+		return this.toString().hashCode();
+	}
+
+	boolean isPerfect() {
+		UnionFind uf = new UnionFind(height*width);
+
+		// union find cycle detection
+		for(int i = 0; i < height; ++i) {
+			// horizontal edges
+			for(int j = 0; j < width-1; ++j) {
+				if(grid[i][j].hasPassageTo(grid[i][j+1])) {
+					if(uf.sameClass(coordToInt(i,j), coordToInt(i,j+1)))
+						return false;
+					uf.union(coordToInt(i,j), coordToInt(i,j+1));
+				}
+			}
+
+			// there are no vertical edges in last row, so we're done
+			if(i == height-1)
+				continue;
+
+			// vertical edges
+			for(int j = 0; j < width; ++j) {
+				if(grid[i][j].hasPassageTo(grid[i+1][j])) {
+					if(uf.sameClass(coordToInt(i,j), coordToInt(i+1,j)))
+						return false;
+					uf.union(coordToInt(i,j), coordToInt(i+1,j));
+				}
+			}
+		}
+
+		// check if connected
+		return (uf.getSize(0) == height*width);
+	}
+
+	void clearMarks() {
+		for (Cell[] row : grid)
+			for (Cell c : row)
+				c.setMarked(false);
+	}
+}
+
